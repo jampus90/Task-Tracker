2637a6f95db58eb9276ce9404c7b22e378ee73f3
jampus90 <alexandre.yama2@gmail.com>
2025-10-15 10:46:57 -0300

    Feature: Implement CRUD

---
+++ Task Tracker/CRUDManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_Tracker
+{
+    internal class CRUDManager
+    {
+        private string path = "todo.json";
+        public string CreateTask(string description)
+        {
+            List<TaskProperties> tasks = new List<TaskProperties>();
+
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, JsonSerializer.Serialize(tasks));
+            }
+
+            string jsonData = File.ReadAllText(path);
+            tasks = JsonSerializer.Deserialize<List<TaskProperties>>(jsonData) ?? new List<TaskProperties>();
+
+            int newId = tasks.Count > 0 ? tasks.Max(t => t.Id) + 1 : 1;
+            TaskProperties newTask = new TaskProperties
+            {
+                Id = newId,
+                Description = description,
+                Status = "Not started",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+
+            tasks.Add(newTask);
+
+            File.WriteAllText(path, JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true }));
+            Console.WriteLine(Path.GetFullPath(path));
+
+
+            return $"Task '{description}' created!";
+        }
+
+        public string UpdateTask(int id, string newDescription = null, string newStatus = null)
+        {
+            if (!File.Exists(path))
+            {
+                return "File not found.";
+            }
+
+            string jsonData = File.ReadAllText(path);
+            List<TaskProperties> tasks = JsonSerializer.Deserialize<List<TaskProperties>>(jsonData) ?? new List<TaskProperties>();
+
+            TaskProperties taskToUpdate = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (taskToUpdate == null)
+            {
+                return "Task not found";
+            }
+
+            if (!string.IsNullOrWhiteSpace(newDescription))
+            {
+                taskToUpdate.Description = newDescription;
+            }
+
+            if (!string.IsNullOrWhiteSpace(newStatus))
+            {
+                taskToUpdate.Status = newStatus;
+            }
+
+            taskToUpdate.UpdatedAt = DateTime.Now;
+
+            File.WriteAllText(path, JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true }));
+
+            return $"Task {taskToUpdate.Id} updated!";
+        }
+
+        public string DeleteTask(int id)
+        {
+            if (!File.Exists(path))
+            {
+                return "File not found";
+            }
+
+            string jsonData = File.ReadAllText(path);
+            List<TaskProperties> tasks = JsonSerializer.Deserialize<List<TaskProperties>>(jsonData) ?? new List<TaskProperties>();
+
+            TaskProperties taskToDelete = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (taskToDelete == null)
+            {
+                return "Task not found.";
+            }
+
+            tasks.Remove(taskToDelete);
+
+            File.WriteAllText(path, JsonSerializer.Serialize(tasks, new JsonSerializerOptions { WriteIndented = true }));
+
+            return $"Task '{taskToDelete.Description}' deleted successfully.";
+        }
+
+        public List<TaskProperties> ListAllTasks()
+        {
+            if (!File.Exists(path))
+            {
+                return new List<TaskProperties>();
+            }
+
+            string jsonData = File.ReadAllText(path);
+            List<TaskProperties> tasks = JsonSerializer.Deserialize<List<TaskProperties>>(jsonData) ?? new List<TaskProperties>();
+
+            return tasks;
+        }
+    }
+}
--- Task Tracker/Program.cs
+++ Task Tracker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace Task_Tracker
     {
         static void Main(string[] args)
         {
+            CRUDManager crudManager = new CRUDManager();
+
             Console.WriteLine("Welcome to your ToDo List!");
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("" +
@@ -23,26 +26,49 @@ namespace Task_Tracker
 
             int selectedOption = Convert.ToInt32(Console.ReadLine());
 
+
             switch(selectedOption)
             {
                 case 0:
-                    Console.WriteLine("Task adicionada");
+                    Console.WriteLine("Type your new task description: ");
+                    string description = Console.ReadLine();
+                    string createTask = crudManager.CreateTask(description);
                     break;
+
                 case 1:
-                    Console.WriteLine("Task atualizada");
+                    Console.WriteLine("Which task do you want o update:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Whats the new description?");
+                    string newDescription = Console.ReadLine();
+
+                    Console.WriteLine("What's the new status?");
+                    string newStatus = Console.ReadLine();
+
+                    string updateTask = crudManager.UpdateTask(id, newDescription, newStatus);
                     break;
+
                 case 2:
-                    Console.WriteLine("Task deletada");
+                    Console.WriteLine("Which task do you want to delete?");
+                    id = Convert.ToInt32(Console.ReadLine());
+
+                    string deleteTask = crudManager.DeleteTask(id);
                     break;
+
                 case 3:
-                    Console.WriteLine("Listagem de todas task");
+                    List<TaskProperties> listTasks = crudManager.ListAllTasks();
+                    string json = JsonSerializer.Serialize(listTasks, new JsonSerializerOptions { WriteIndented = true });
+                    Console.WriteLine(json);
                     break;
+
                 case 4:
                     Console.WriteLine("Listagem de task feitas");
                     break;
+
                 case 5:
                     Console.WriteLine("Listagem de tasks não iniciadas");
                     break;
+
                 case 6:
                     Console.WriteLine("Listagem de tasks em progresso");
                     break;
